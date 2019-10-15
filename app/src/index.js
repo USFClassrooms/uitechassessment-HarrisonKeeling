b275598800d2c9e59dfb792b340089fe877b3db9
Harrison Keeling <chasekeeling@me.com>
2019-10-15 14:57:27 -0700

    Add redux and boilerplate root saga and reducer

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer from './rootReducer';
+import rootSaga from './rootSaga';
+import App from './app';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
+
